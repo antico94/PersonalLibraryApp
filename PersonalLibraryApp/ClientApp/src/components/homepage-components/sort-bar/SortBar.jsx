60de95ec1ff34c82d82cfeb67063c808df56fb27
antico94 <meiuandrei@gmail.com>
2023-02-04 16:54:05 +0200

    Finished Final Aesthetics SortBar

--- PersonalLibraryApp/ClientApp/src/components/homepage-components/sort-bar/SortBar.jsx
+++ PersonalLibraryApp/ClientApp/src/components/homepage-components/sort-bar/SortBar.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './SortBar.css'
+import ArrowDown from './../../../assets/svg/arrowDown.svg'
 
 const SortBar = () => {
+    const optionsAvailable = ['Popular', 'Recent']
     return (
-        <div>
-
+        <div className='sortBar'>
+            <div className="sortLine"></div>
+            <div className="sortText">
+                <p>Sort after:</p>
+                <div className='sortBarCriteria'>
+                    <span>{optionsAvailable[0]}</span>
+                    <img src={ArrowDown} about='Arrow_Down'/>
+                </div>
+            </div>
         </div>
     );
 };
