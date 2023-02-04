247ec6edb9a838c342d9f4357cb34d8af7bafb2e
antico94 <meiuandrei@gmail.com>
2023-02-04 12:26:21 +0200

    Removed Useless packages / Added New Search Bar

--- PersonalLibraryApp/ClientApp/src/components/individual-components/nav/NavMenu.jsx
+++ PersonalLibraryApp/ClientApp/src/components/individual-components/nav/NavMenu.jsx
@@ -5,33 +5,36 @@ import SearchIcon from './../../../assets/svg/search.svg'
 import {Link, useNavigate} from 'react-router-dom';
 
 const NavMenu = () => {
-      const navigate = useNavigate();
-  const handleLogoClick = () => {
-    navigate('/');
-  };
+    const navigate = useNavigate();
+    const handleLogoClick = () => {
+        navigate('/');
+    };
 
-  const handleProfileClick = () => {
-    navigate('/login');
-  };
+    const handleProfileClick = () => {
+        navigate('/login');
+    };
     return (
         <div className='navBar'>
             <div className="actualNav">
                 <div className="actualNavItems">
-                    <img src={Logo}/>
-                    <div className="search-container">
-                        <div className="search-icon"></div>
-                        <input type="text" className="search-input" placeholder="Search"/>
-                        <Link className="navBarLink" tag={Link} to="/intro">Intro</Link>
-                        <Link className="navBarLink" tag={Link} to="/">Book finder</Link>
-                        <Link className="navBarLink" tag={Link} to="/">Popular</Link>
-                        <Link className="navBarLink" tag={Link} to="/">Portfolio</Link>
-                        <Link className="navBarLink" tag={Link} to="/">Community</Link>
-
+                    <div className="navStart">
+                        <img src={Logo}/>
+                        <div className="search-bar">
+                            <input type="text" placeholder="Search"/>
+                        </div>
+                    </div>
+                    <div className="navLinks">
+                        <Link className="navBarLink" tag={Link} to="/">Home</Link>
+                        <Link className="navBarLink" tag={Link} to="/">Friends</Link>
+                        <Link className="navBarLink" tag={Link} to="/">Notifications</Link>
+                        <Link className="navBarLink" tag={Link} to="/">Messages</Link>
+                        <Link className="navBarLink" tag={Link} to="/">Book Swap</Link>
                     </div>
                 </div>
             </div>
         </div>
-    );
+    )
+        ;
 };
 
 export default NavMenu;
