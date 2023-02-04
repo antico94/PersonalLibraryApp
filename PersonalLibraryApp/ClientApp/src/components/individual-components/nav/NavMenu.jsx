6d8e363d287980d89fa2fdbff66c66e703ba6c73
antico94 <meiuandrei@gmail.com>
2023-02-04 16:10:40 +0200

    Finished Final Aesthetics NavBar

--- PersonalLibraryApp/ClientApp/src/components/individual-components/nav/NavMenu.jsx
+++ PersonalLibraryApp/ClientApp/src/components/individual-components/nav/NavMenu.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import './NavMenu.css'
 import Logo from './../../../assets/images/logo/Logo.png'
-import SearchIcon from './../../../assets/svg/search.svg'
+import Friends from './../../../assets/svg/friends.svg'
+import Notification from './../../../assets/svg/notification.svg'
+import Message from './../../../assets/svg/message.svg'
+import BookSwap from './../../../assets/svg/BookSwap.svg'
+import Home from './../../../assets/svg/home.svg'
+import Profile from './../../../assets/svg/avatar.svg'
+import ArrowDown from './../../../assets/svg/arrowDown.svg'
 import {Link, useNavigate} from 'react-router-dom';
 
 const NavMenu = () => {
@@ -24,11 +30,34 @@ const NavMenu = () => {
                         </div>
                     </div>
                     <div className="navLinks">
-                        <Link className="navBarLink" tag={Link} to="/">Home</Link>
-                        <Link className="navBarLink" tag={Link} to="/">Friends</Link>
-                        <Link className="navBarLink" tag={Link} to="/">Notifications</Link>
-                        <Link className="navBarLink" tag={Link} to="/">Messages</Link>
-                        <Link className="navBarLink" tag={Link} to="/">Book Swap</Link>
+                        <div className="navHome">
+                            <img className='navLinksImages' src={Home} alt='Home_Icon'/>
+                            <Link className="navBarLink" tag={Link} to="/">Home</Link>
+                        </div>
+                        <div className="navFriends">
+                            <img className='navLinksImages' src={Friends} alt='Friends_Icon'/>
+                            <Link className="navBarLink" tag={Link} to="/">Friends</Link>
+                        </div>
+
+
+                        <div className="navBookSwap">
+                            <img className='navLinksImages' src={BookSwap} alt='Book_Icon'/>
+                            <Link className="navBarLink" tag={Link} to="/">Book Swap</Link>
+                        </div>
+                        <div className="navMessages">
+                            <img className='navLinksImages' src={Message} alt='Message_Icon'/>
+                            <Link className="navBarLink" tag={Link} to="/">Messages</Link>
+                        </div>
+                        <div className="navNotifications">
+                            <img className='navLinksImages' src={Notification} alt='Notification_Icon'/>
+                            <Link className="navBarLink" tag={Link} to="/">Notifications</Link>
+                        </div>
+                        <div className="navProfile">
+                            <img className='navLinksImages' id='navLinkImageNoFilter' src={Profile} alt='Profile_Icon'/>
+                            <Link className="navBarLink" tag={Link} to="/">Me</Link>
+                            <img id='navProfileDownArrow' src={ArrowDown} alt='Down_Arrow'/>
+
+                        </div>
                     </div>
                 </div>
             </div>
